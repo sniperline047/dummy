355ae140dcc37690517fc6f6a2a7e58513b21b36
rohanleo <rohansingh726@gmail.com>
2020-05-05 20:27:28 +0530

    initial commit

---
+++ app/src/main/java/com/example/dummy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.dummy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (! Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+    }
+}
